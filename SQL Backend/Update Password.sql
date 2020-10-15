3ffe835a068378e660e2340289c313dab7768f0a
Sebastien <spierrelouis@ggc.edu>
2020-10-14 22:24:47 -0400

    fixing github

--- SQL Backend/Update Password.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR ALTER PROCEDURE  reset_password
-@new_password VARCHAR(255),
-@username VARCHAR(255)
-AS
-UPDATE users
-SET user_password = @new_password
-WHERE username = @username;
-GO
--- SQL Backend/Update Username.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR ALTER PROCEDURE  update_username
-@new_username VARCHAR(255),
-@username VARCHAR(255)
-AS
-UPDATE users
-SET username = @new_username
-WHERE username = @username;
-GO
